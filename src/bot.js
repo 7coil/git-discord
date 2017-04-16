fe7cfac9ee2a24e4d342f8a8b39a1f1de78908f8
7coil <leondrolio@gmail.com>
2017-04-16 01:43:26 +0100

    Add forwardslash!

--- src/bot.js
+++ src/bot.js
@@ -199,7 +199,7 @@ client.on("message", function(message) {
 			});
 
 	} else if(input[1] === "help" || input[1] === "--help") {
-		return message.channel.sendMessage("`git-gud bot by 7coil, http://moustacheminer.com`");
+		return message.channel.sendMessage("`git-gud bot by 7coil, http://moustacheminer.com/`");
 	}
 
 	//Check if it's an invalid git command
